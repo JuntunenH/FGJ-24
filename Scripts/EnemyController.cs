b91c74b7758ba9cf83120c79f82b392dce79c1e1
Pussmarr <hodari86@hotmail.com>
2024-01-26 20:05:07 +0200

    Playercontroller scripts and paint changes

--- Scripts/EnemyController.cs
+++ Scripts/EnemyController.cs
@@ -1,39 +1,35 @@
 using Godot;
 using System;
-
-public partial class Enemy : CharacterBody2D
+public partial class EnemyController : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	// Enemy stats
+	[ExportCategory("Enemy Variables")]
+	//public const uint MaxLifepoints = 100; 
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+ 	[Export]
+    public int Lifepoints { get; set; } = 100;//(int)MaxLifepoints;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+    // [Export]
+    // public uint Damage { get; set; } = 5;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+    // [Export]
+    // public float MovementSpeed = 4;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+    // [Export]
+    // public int Experience { get; private set; } = 1;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+    // private void Attack()
+    // {
+    //     if (!IsPlayerInAttackRange) return;
+    //     _attackCooldown.Start();
+    //     _gameManager.Player.TakeDamages(Damages);
+    // }
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+    // internal void TakeDamage(uint damages = 1)
+    // {
+    //     EmitSignal(SignalName.OnEnemyHit, this, damages);
+    //     Lifepoints -= (int)damages;
+    //     if (Lifepoints > 0) return;
+    //     QueueFree();
+    // }
 }
