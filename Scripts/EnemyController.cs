0bda131b3f7b57bf5741444d39ea92088cfb18a2
Pussmarr <hodari86@hotmail.com>
2024-01-26 21:02:39 +0200

    More good grass

--- Scripts/EnemyController.cs
+++ Scripts/EnemyController.cs
@@ -9,14 +9,29 @@ public partial class EnemyController : CharacterBody2D
  	[Export]
     public int Lifepoints { get; set; } = 100;//(int)MaxLifepoints;
 
-    // [Export]
-    // public uint Damage { get; set; } = 5;
+    [Export]
+    public uint Damage { get; set; } = 5;
 
-    // [Export]
-    // public float MovementSpeed = 4;
+    [Export]
+    public float MovementSpeed = 4;
 
-    // [Export]
-    // public int Experience { get; private set; } = 1;
+    [Export]
+    public int Experience { get; private set; } = 1;
+
+    private Timer _attackCooldown;
+    private GameManager _gameManager;
+
+    public override void _Ready()
+    {
+        _gameManager = GetNode<GameManager>("/root/GameManager");
+        _attackCooldown = GetNode<Timer>("AttackCooldown");
+        // var hud = GetNode<Control>("../HUD");
+        // _lifebar = GD.Load<PackedScene>("res://Prefabs/UI/enemy_life_bar.tscn").Instantiate<ProgressBar>();
+        // _lifebar.MaxValue = Lifepoints;
+        // hud.AddChild(_lifebar);
+    }
+
+
 
     // private void Attack()
     // {
