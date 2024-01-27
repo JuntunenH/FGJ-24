d1062541073308595b98628b02463d201f4d69d6
Pussmarr <hodari86@hotmail.com>
2024-01-28 01:08:20 +0200

    EnemyController cleanup

--- Scripts/Enemies/EnemyController.cs
+++ Scripts/Enemies/EnemyController.cs
@@ -22,21 +22,15 @@ public partial class EnemyController : CharacterBody2D
 
 	[Export]
 	public float MovementSpeed = 40;
-
-	[Export]
-	public int Experience { get; private set; } = 1;
 	[Export]
 	public PackedScene Pickup;
 
 	private Timer _attackCooldown;
 	private CharacterController _player;
-	Sprite2D e_sprite;
 
 	public override void _Ready()
 	{
 		_player = GetNode<CharacterController>("/root/World/PlayerCharacter");
-		// fetch player sprite
-		e_sprite = GetNode<Sprite2D>("Sprite2D");
 	}
 	public override void _PhysicsProcess(double delta)
 	{
@@ -45,12 +39,6 @@ public partial class EnemyController : CharacterBody2D
 		var direction = (playerPos - GlobalPosition).LimitLength();
 		Velocity = direction * MovementSpeed;
 		MoveAndSlide();
-        for (int i = 0; i < GetSlideCollisionCount(); i++)
-        {
-            var collision = GetSlideCollision(i);
-            GD.Print("I collided with ", ((Node)collision.GetCollider()).Name);
-            _player.TakeDamage(Damage);
-        }
 	}
 	public void TakeDamage(int amount){
 		GD.Print("Enemy take damage ",amount);
