5cb2e5e6ded01e45901970edcf8705ec01579e1d
Pussmarr <hodari86@hotmail.com>
2024-01-28 11:52:01 +0200

    First audio

--- Scripts/Pickups/money.cs
+++ Scripts/Pickups/money.cs
@@ -4,10 +4,13 @@ using System;
 public partial class money : Area2D
 {
 	private CharacterController _player;
+	private AudioStreamPlayer2D _sound;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_player = GetNode<CharacterController>("/root/World/PlayerCharacter");
+		_sound = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+		_sound.Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
